f9076bd5e5b2f2b7c60ae2db9708fe4a2417559c
Klemens <klemenswiyanto@yahoo.com>
2019-09-18 15:07:13 +0700

    Update Changes To Finding Star
    Change the box size

--- Meeting04-FindingStar/app/src/main/java/com/github/gmarcell/tryyourluck/MainActivity.kt
+++ Meeting04-FindingStar/app/src/main/java/com/github/gmarcell/tryyourluck/MainActivity.kt
@@ -12,38 +12,33 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-    val OnID = 17301516
-    val OffID = 17301513
-    lateinit var ViewSelect: View
-    var IDSelect = 1
+    private lateinit var binding: ActivityMainBinding
+    private val onid = 17301516
+    private val offid = 17301513
+    private lateinit var ViewSelect: View
+    private var idselect = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val box: List<Int> = listOf(
-            binding.textBox1.id, binding.textBox2.id,
-            binding.textBox3.id, binding.textBox4.id,
-            binding.textBox5.id, binding.textBox6.id,
-            binding.textBox7.id, binding.textBox8.id,
-            binding.textBox9.id, binding.textBox10.id,
-            binding.textBox11.id, binding.textBox12.id,
-            binding.textBox13.id, binding.textBox14.id,
-            binding.textBox15.id, binding.textBox16.id
+            binding.textBox1.id, binding.textBox2.id, binding.textBox3.id,
+            binding.textBox4.id, binding.textBox5.id, binding.textBox6.id,
+            binding.textBox7.id, binding.textBox8.id, binding.textBox9.id
         )
-        IDSelect = box[Random.nextInt(0, 16)]
+        idselect = box[Random.nextInt(0, 8)]
         setListeners()
     }
 
     private fun setLogic(view: View) {
-        if(view.id == IDSelect) {
-            setBackground(view, OnID)
+        if(view.id == idselect) {
+            setBackground(view, onid)
             ViewSelect = view
             Toast.makeText(this, "YOU LUCKY!!! Press The Star to Restart", Toast.LENGTH_LONG).show()
             ViewSelect.setOnClickListener {
                 reset()
             }
-        } else setBackground(view, OffID)
+        } else setBackground(view, offid)
     }
 
     private fun setBackground(view: View, buttonIcon: Int) {
@@ -53,16 +48,11 @@ class MainActivity : AppCompatActivity() {
     private fun reset() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val box: List<Int> = listOf(
-            binding.textBox1.id, binding.textBox2.id,
-            binding.textBox3.id, binding.textBox4.id,
-            binding.textBox5.id, binding.textBox6.id,
-            binding.textBox7.id, binding.textBox8.id,
-            binding.textBox9.id, binding.textBox10.id,
-            binding.textBox11.id, binding.textBox12.id,
-            binding.textBox13.id, binding.textBox14.id,
-            binding.textBox15.id, binding.textBox16.id
+            binding.textBox1.id, binding.textBox2.id, binding.textBox3.id,
+            binding.textBox4.id, binding.textBox5.id, binding.textBox6.id,
+            binding.textBox7.id, binding.textBox8.id, binding.textBox9.id
         )
-        IDSelect = box[Random.nextInt(0, 16)]
+        idselect = box[Random.nextInt(0, 8)]
 
         val textBox1 = findViewById<TextView>(R.id.text_box1)
         val textBox2 = findViewById<TextView>(R.id.text_box2)
@@ -73,17 +63,10 @@ class MainActivity : AppCompatActivity() {
         val textBox7 = findViewById<TextView>(R.id.text_box7)
         val textBox8 = findViewById<TextView>(R.id.text_box8)
         val textBox9 = findViewById<TextView>(R.id.text_box9)
-        val textBox10 = findViewById<TextView>(R.id.text_box10)
-        val textBox11 = findViewById<TextView>(R.id.text_box11)
-        val textBox12 = findViewById<TextView>(R.id.text_box12)
-        val textBox13 = findViewById<TextView>(R.id.text_box13)
-        val textBox14 = findViewById<TextView>(R.id.text_box14)
-        val textBox15 = findViewById<TextView>(R.id.text_box15)
-        val textBox16 = findViewById<TextView>(R.id.text_box16)
 
         val clickableViews: List<View> =
             listOf(textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7, textBox8,
-                textBox9, textBox10, textBox11, textBox12, textBox13, textBox14, textBox15, textBox16)
+                textBox9)
 
         for (item in clickableViews) {
             item.setOnClickListener {
@@ -103,17 +86,10 @@ class MainActivity : AppCompatActivity() {
         val textBox7 = findViewById<TextView>(R.id.text_box7)
         val textBox8 = findViewById<TextView>(R.id.text_box8)
         val textBox9 = findViewById<TextView>(R.id.text_box9)
-        val textBox10 = findViewById<TextView>(R.id.text_box10)
-        val textBox11 = findViewById<TextView>(R.id.text_box11)
-        val textBox12 = findViewById<TextView>(R.id.text_box12)
-        val textBox13 = findViewById<TextView>(R.id.text_box13)
-        val textBox14 = findViewById<TextView>(R.id.text_box14)
-        val textBox15 = findViewById<TextView>(R.id.text_box15)
-        val textBox16 = findViewById<TextView>(R.id.text_box16)
 
         val clickableViews: List<View> =
-            listOf(textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7, textBox8,
-                textBox9, textBox10, textBox11, textBox12, textBox13, textBox14, textBox15, textBox16)
+            listOf(textBox1, textBox2, textBox3, textBox4, textBox5,
+                textBox6, textBox7, textBox8, textBox9)
 
         for (item in clickableViews) {
             item.setOnClickListener {
